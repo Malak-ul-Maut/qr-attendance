3316bfe82ab5bb176506fd73615d75fcfe360907
Ahad Ali <logintemporarily@gmail.com>
2025-11-30 22:13:36 +0530

    Modify generate-cert.js to take server ip dynamically

--- backend/generate-cert.js
+++ backend/generate-cert.js
@@ -1,9 +1,10 @@
-// generate-cert.js
 const fs = require('fs');
+const os = require('os');
 const path = require('path');
 const selfsigned = require('selfsigned');
+const serverIp = getServerIpAddress();
 
-const attrs = [{ name: 'commonName', value: '10.138.132.200' }]; // your local IP
+const attrs = [{ name: 'commonName', value: `${serverIp}` }]; // your local IP
 const pems = selfsigned.generate(attrs, {
   days: 365,
   keySize: 2048,
@@ -15,3 +16,18 @@ fs.writeFileSync(path.join(certDir, 'cert.pem'), pems.cert);
 fs.writeFileSync(path.join(certDir, 'key.pem'), pems.private);
 
 console.log('✅ Certificate generated successfully: cert.pem and key.pem');
+
+
+function getServerIpAddress() {
+  const networkInterfaces = os.networkInterfaces();
+  for (const interfaceName in networkInterfaces) {
+    const addresses = networkInterfaces[interfaceName];
+    for (const address of addresses) {
+      // Filter for IPv4 addresses that are not internal (loopback)
+      if (address.family === 'IPv4' && !address.internal) {
+        return address.address;
+      }
+    }
+  }
+  return null; 
+}
