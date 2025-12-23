d002269656581a699168ce3d55753a13033b6305
Ahad Ali <logintemporarily@gmail.com>
2025-12-23 18:15:07 +0530

    feat: implement tunnel hosting in server initialization via ngrok js sdk

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const https = require('https');
 const { Server } = require('socket.io');
 const os = require('os');
 const selfsigned = require('selfsigned');
+const ngrok = require('@ngrok/ngrok');
 
 // ----------------- Server Config -----------------
 const express = require('express');
@@ -22,11 +23,9 @@ app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/session', require('./routes/session.routes'));
 app.use('/api/attendance', require('./routes/attendance.routes'));
 
-initializeServer();
-
 // ----------------- Functions ---------------------
 
-async function initializeServer() {
+(async function initializeServer() {
   // Generate a self-signed certificate
   const attrs = [{ name: 'commonName', value: 'localhost' }];
   const pems = await selfsigned.generate(attrs, {
@@ -40,7 +39,48 @@ async function initializeServer() {
   };
   const server = https.createServer(options, app);
 
-  // Initialize Socket.io server
+  initializeSocket(server); // Initialize Socket.io server
+
+  // Serve frontend
+  const FRONTEND_DIR = path.join(__dirname, '../frontend');
+  app.use(express.static(FRONTEND_DIR));
+
+  // If someone hits a route that's not an API (fallback)
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(FRONTEND_DIR, 'homepage.html'));
+  });
+
+  // --------------- Start server --------------
+  const serverIp = getServerIpAddress();
+  const PORT = 4000;
+
+  server.listen(PORT, () =>
+    console.log(`🚀 Server running at https://${serverIp}:${PORT}`),
+  );
+
+  // Host tunnel online
+  hostTunnel(`https://${serverIp}:${PORT}`);
+})();
+
+async function hostTunnel(url) {
+  const listener = await ngrok.forward({
+    addr: url,
+    authtoken: '34KSMFr7qqWSRNPIpbD2bXZwKvT_6cKTDHNoT8J1tP5Byge7T',
+    verify_upstream_tls: false,
+  });
+  console.log(`Tunnel established at: ${listener.url()}`);
+}
+
+function getServerIpAddress() {
+  return (
+    Object.values(os.networkInterfaces())
+      .flat()
+      .find(details => details.family === 'IPv4' && !details.internal)
+      ?.address || null
+  );
+}
+
+function initializeSocket(server) {
   const io = new Server(server, {
     cors: { origin: '*', methods: ['GET', 'POST'] },
   });
@@ -53,30 +93,4 @@ async function initializeServer() {
       );
     });
   });
-
-  // Serve frontend
-  const FRONTEND_DIR = path.join(__dirname, '../frontend');
-  app.use(express.static(FRONTEND_DIR));
-
-  // If someone hits a route that's not an API (fallback)
-  app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(FRONTEND_DIR, 'homepage.html'));
-  });
-
-  // Start server
-  const serverIp = getServerIpAddress();
-  const PORT = 4000;
-
-  server.listen(PORT, () =>
-    console.log(`🚀 Server running at https://${serverIp}:${PORT}`),
-  );
-}
-
-function getServerIpAddress() {
-  return (
-    Object.values(os.networkInterfaces())
-      .flat()
-      .find(details => details.family === 'IPv4' && !details.internal)
-      ?.address || null
-  );
 }
